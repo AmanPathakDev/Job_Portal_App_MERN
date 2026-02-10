66a605b8c108610c32dec912903643304eabfb80
apktvchannel <apktvchannel@gmail.com>
2026-02-11 02:36:44 +0530

    Created controllers

---
+++ backend/routes/application.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import {
+  applyJob,
+  getApplicants,
+  getAppliedJobs,
+  updateStatus,
+} from "../controllers/application.controller.js";
+
+const router = express.Router();
+
+router.route("/apply/:id").post(isAuthenticated, applyJob);
+router.route("/get").post(isAuthenticated, getAppliedJobs);
+router.route("/:id/applicants").post(isAuthenticated, getApplicants);
+router.route("/status/:id/update").post(isAuthenticated, updateStatus);
+
+export default router;
